29849a178d0324d647f5a5e1c962501f8b673a2d
Waldanag <walter@algabits.com>
2023-03-07 00:04:14 -0300

    Agregando Arrays

---
+++ arrays.js
@@ -0,0 +1,53 @@
+// let str = "un string cualquiera"
+// let arrayAleatorio= ["Digital", "House", "true", "string", "123","false", "54", str ]
+// console.log(arrayAleatorio[arrayAleatorio.length - 1])
+
+let peliculas = ["star wars", "totoro", "rocky", "pulp fiction", "la vida es bella"]
+let peluculasAnimadas = ["toy story", "finding nemo", "kung-fu panda", "wally", "fortnite"]
+const asiaScores = [8, 10, 6, 9, 10, 6, 6, 8, 4];
+const euroScores = [8, 10, 6, 8, 10, 6, 7, 9, 5];
+
+//TRANSFORMAR TODO UN ARRAY A MAYUSCULAS
+const minToMayusculas = (array) => {
+
+    for (let i = 0; i < array.length; i++) {
+        array[i] = array[i].toUpperCase()
+    }
+    return array
+}
+
+//AGREGAR UN ARRAY A OTRO ARRAY
+const agregarPeliculas = () => {
+
+    peliculas = [].concat(peliculas, peluculasAnimadas)
+    return peliculas
+
+}
+agregarPeliculas()
+
+let nuevoArray = minToMayusculas(peliculas)
+console.log(nuevoArray);
+
+//SACAR EL ULTIMO ELEMENTO YA QUE NO CORRESPONDE AL GRUPO
+let videoJuegos = peliculas.pop()
+console.log(videoJuegos);
+
+//COMPARAR SI 2 ARRAYS SON IGUALES O NO, EN ESTE CASO LAS CALIFICACIONES
+const comparacionNotas = (a, b) => {
+
+    if (a.length !== b.length) return false;
+    else {
+        for (var i = 0; i < a.length; i++) {
+            if (a[i] !== b[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+//TAREA ENTENDER MEJOR LA FUNCION DE COMPARACION 
+let notaPeliculas = comparacionNotas(asiaScores, euroScores)
+console.log(notaPeliculas);
+
+
